8ac40e7f8a7c214acebf1e292b4c244d1562e29b
DragonKingA <2472062511@qq.com>
2023-07-21 00:21:44 +0800

    cpp

--- codeofCpp/temp.cpp
+++ codeofCpp/temp.cpp
@@ -4,108 +4,24 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(), v.end()
 const int N = 2e5 + 10;
-int n, T, m;
-ll ans = 0;
-
-
-void dfs(int i)
-{
-    
-}
 
 void Solve()
 {
-    cin >> n >> T >> m;
-    for(int i = 0; i < m; ++i)
-    {
-        int a, b; cin >> a >> b;
-        
-    }
-    
-   
-    
+	int n; cin >> n;
+	vector<int> v(n);
+	for(int &x : v) cin >> x;
+	
 }
 
 int main()
 {
-    // untie();
-    int _ = 1;
-    // int T = 1;
-    // cin >> T;
-    while(_--)
+    untie();
+    int T = 1;
+    // int T = 100;
+    cin >> T;
+    while(T--)
     {
         Solve();
     }
     return 0;
 }
-
-
-// #include <bits/stdc++.h>
-// using namespace std;
-// #define untie() {cin.tie(0)->sync_with_stdio(false), cout.tie(0);}
-// #define ll long long
-// #define all(v) v.begin(), v.end()
-// const int N = 100 + 10;
-// const ll mod = 998244353LL;
-// int n;
-// ll a[N], dp[15];
-
-// ll quick_pow(ll base, ll index = mod - 2)
-// {
-//     ll ans = 1;
-//     base %= mod;
-//     for(; index; index >>= 1)
-//     {
-//         if(index & 1) ans = ans * base % mod;
-//         base = base * base % mod;
-//     }
-//     return ans % mod;
-// }
-
-// void Solve()
-// {
-//     cin >> n;
-//     for(int i = 1; i <= n; ++i) 
-//     {
-//         cin >> a[i];
-//         for(int j = 1; j <= min(a[i], 10LL); ++j)
-//         {
-//             dp[j] = (dp[j] + quick_pow(a[i])) % mod;
-//         }
-//     }
-//     // for(int i = 2; i <= 10; ++i)
-//     // {
-//     //     for(int j = 1; j < i; ++j)
-//     //     {
-//     //         // if(i - j == j) continue;
-//     //         dp[i] = (dp[i] + quick_pow(dp[i - j] * dp[j])) % mod;
-//     //     }
-//     // }
-//     for(int i = 1; i <= 10; ++i)
-//     {
-//         for(int j = 1; j <= 10; ++j)
-//         {
-//             // if(i - j == j) continue;
-//             dp[i + j] = (dp[i + j] + quick_pow(dp[i] * dp[j])) % mod;
-//         }
-//     }
-//     for(int i = 2; i <= 10; ++i)
-//     {
-//         if(i & 1) continue;
-//         dp[i] = (dp[i] - dp[i / 2] + mod) % mod;
-//     }
-//     cout << dp[10] % mod << "\n";
-// }
-
-// int main()
-// {
-//     // untie();
-//     int _ = 1;
-//     // int T = 1;
-//     // cin >> T;
-//     while(_--)
-//     {
-//         Solve();
-//     }
-//     return 0;
-// }
--- codeofCpp/templates.cpp
+++ codeofCpp/templates.cpp
@@ -29,14 +29,20 @@ int main()
 }
 
 */
+
+// 最大独立集 + 基环树 + 并查集
+// 所给图没有规定是棵树，可能含有环和多个连通块，需要并查集统计全部连通块并逐个dfs求解。而每个连通块内可能含有环，需要进行断环操作：即把
+// 环找出来后从中间某边断开，分别以断边的两个端点为根做一次树形dp（最大独立集），取最大者累加到答案中。
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int N = 1e5 + 10, M = 2 * N;
+#define inf 0x7fffffff
+const int N = 1e6 + 10, M = 2 * N;
 
-int n, m, cnt = 1;
-int head[N];
-int dp[N][N];
+int n, m, k, cnt = 1;
+int head[N], ds[N], a[N];
+bool vis[N];
+ll dp[N][2], ans = 0;
 
 template <class T = int>
 class Edge
@@ -56,31 +62,52 @@ class Edge
 
 Edge<int> e[M];
 
-void dfs(int u, int fa = 0)
+void dfs(int u, int root)
 {
-
+    vis[u] = 1; // 标记整个连通块
+    dp[u][1] = a[u];
+    dp[u][0] = 0;
     for(int i = head[u]; i; i = e[i].next)
     {
-        int v = e[i].to, w = e[i].w, t = 0;
-        if(v == fa) continue;
-        dfs(v, u);
-        
-
+        int v = e[i].to;
+        if(v == root) // 成环时防止重复遍历
+        {
+            dp[root][1] = -inf; // 因为是断边操作（减去 u -> root 这条边），则dfs过程中父节点 u 不能选择 root，因此把选择 root 的价值设为无穷小
+            continue;
+        }
+        dfs(v, root);
+        dp[u][0] += max(dp[v][0], dp[v][1]);
+        dp[u][1] += dp[v][0];
     }
+}
+
+void work(int u)
+{
+    while(!vis[ds[u]]) // 向上寻找连通块的根 u
+    {
+        vis[u] = 1;
+        u = ds[u]; 
+    }
+    dfs(u, u);
+    ll tp = max(dp[u][0], dp[u][1]);
+    u = ds[u]; // 如果是环，必定首尾相接，这里获得尾部编号
+    dfs(u, u);
+    ans += max(tp, max(dp[u][0], dp[u][1])); // 取两次 dfs 答案的最大者累计
 }
 
 int main()
 {
-    cin >> n >> m;
-    for(int i = 1; i < n; ++i)
+    cin >> n;
+    for(int i = 1; i <= n; ++i) ds[i] = i;
+    for(int v = 1; v <= n; ++v)
     {
-        int u, v;
-        cin >> u >> v;
-        addedge_undirected(e, u, v);
+        int u;
+        cin >> a[v] >> u;
+        addedge(e, u, v);
+        ds[v] = u; // 注意这里值是父节点，方便后续找根
     }
-    for(int i = 1; i <= n; ++i) cin >> dp[i][1];
-
-    dfs(1);
-    
+    for(int i = 1; i <= n; ++i)
+        if(!vis[i]) work(i);
+    cout << ans << "\n";
     return 0;
 }
