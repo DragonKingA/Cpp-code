fb8f0870398021dc83c22b4d910f15cb25807b4e
DragonKingA <2472062511@qq.com>
2023-09-23 02:45:55 +0800

    cpp

--- codeofCpp/temp.cpp
+++ codeofCpp/temp.cpp
@@ -6,41 +6,53 @@ using namespace std;
 #define gcd(a, b) __gcd(a, b)
 #define lcm(a, b) (a) * (b) /__gcd(a, b)
 const int N = 2e5 + 10, inf = 0x7fffffff;
-vector<int> G[N];
-int ds[N];
 
-int find_set(int x)
+vector<int> G[N];
+int n, a, b, root;
+int dep[N], dis[N];
+
+void dfs(int u, int fa = -1)
 {
-    return x == ds[x] ? x : (ds[x] = find_set(ds[x]));
+    for(int v : G[u])
+    {
+        if(dep[v] == -1) dep[v] = dep[u] + 1, dfs(v, u);
+        else if(dep[v] < dep[u] && fa != v) root = v;
+    }
+}
+
+bool Dijkstra()
+{
+    dis[root] = 0;
+    queue<int> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int u = q.front(); q.pop();
+        for(int v : G[u])
+            if(dis[v] == -1)
+                dis[v] = dis[u] + 1, q.push(v);
+    }
+    return dis[a] > dis[b];
 }
 
 void Solve()
 {
-    
-    int n, a, b;
+    root = -1;
     set<int> st;
     cin >> n >> a >> b;
-    for(int i = 0; i <= n; ++i) ds[i] = i;
-    for(int i = 0; i <= n; ++i) G[i].clear();
-    for(int i = 0; i < n; ++i)
+    for(int i = 0; i <= n; ++i) dis[i] = dep[i] = -1;
+    for(int i = 1; i <= n; ++i) G[i].clear();
+    for(int i = 1; i <= n; ++i)
     {
         int u, v;
         cin >> u >> v;
         G[u].push_back(v);
         G[v].push_back(u);
-        int fu = find_set(u), fv = find_set(v);
-        if(fu == fv) st.insert(fu);
-        else ds[fu] = fv;
     }
-    vector<bool> vis(n + 5, 0);
-    for(int i = 1; i <= 7; ++i)
-    {
-        int fi = find_set(i);
-        if(st.find(fi) != st.end()) vis[i] = 1;
-    }
-    for(int i = 1; i <= 7; ++i)
-    cout << i << (vis[i] ? " True" : " False") << "\n";
     if(a == b) return cout << "NO\n", void();
+    dep[b] = 0;
+    dfs(b, -1);
+    cout << (Dijkstra() ? "YES\n" : "NO\n");
 }
 
 int main()
