86557d70860d4069c344b86f4aa2d58477f73706
DragonKingA <2472062511@qq.com>
2023-09-25 01:01:59 +0800

    cpp

--- codeofCpp/temp.cpp
+++ codeofCpp/temp.cpp
@@ -6,18 +6,30 @@ using namespace std;
 #define gcd(a, b) __gcd(a, b)
 #define lcm(a, b) (a) * (b) /__gcd(a, b)
 const int N = 2e5 + 10, inf = 0x7fffffff;
+vector<ll> save;
 
+void dfs(ll x)
+{
+    if(x > 9 && x / 10 % 10 <= x % 10) return;
+    save.push_back(x);
+    dfs(x + 1);
+    dfs(x * 10);
+}
 
 void Solve()
-{
-    
+{  
+    dfs(1);
+    sort(all(save));
+    save.erase(unique(all(save)));
+    int k; cin >> k;
+    cout << save[k - 1] << "\n";
 }
 
 int main()
 {
     untie();
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while(T--)
     {
         Solve();
